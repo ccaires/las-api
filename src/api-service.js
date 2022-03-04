46ef88bb0c0df45085f2e85256bfdcf36f20c4ea
Carolina <caires.c26@gmail.com>
2022-03-04 11:19:59 -0300

    Atividades Essenciais

--- src/api-service.js
+++ src/api-service.js
@@ -1,1 +1,37 @@
-//tetse
+const fetch = require('node-fetch');
+
+
+
+async function listarProdutos(){
+	const responseProdutos = await fetch('https://stupefied-keller-a2c79e.netlify.app/produtos.json');
+	verificarErros(responseProdutos);
+	const dataProdutos = await responseProdutos.json();
+
+	return dataProdutos;
+}
+
+function verificarErros(response){
+	if(response.status !== 200){
+		//throw new Error('${response.statusText}: ${response.status}');
+		throw new Error(`${response.statusText}: ${response.status}`);
+	}
+}
+
+async function listarCategorias(){
+	const responseCategorias = await fetch('https://stupefied-keller-a2c79e.netlify.app/categorias.json');
+	verificarErros(responseCategorias);
+	const dataCategorias = await responseCategorias.json();
+
+	return dataCategorias;
+}
+
+async function listarCupons(){
+	const responseCupons = await fetch('https://stupefied-keller-a2c79e.netlify.app/cupons.json');
+	verificarErros(responseCupons);
+	const dataCupons = await responseCupons.json();
+
+	return dataCupons;
+}
+
+
+module.exports = (listarProdutos, listarCategorias, listarCupons);
